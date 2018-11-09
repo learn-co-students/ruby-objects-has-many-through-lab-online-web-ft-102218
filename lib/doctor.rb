3f26e191244bcf677e333de1b98341d8fab7f15f
Eric Mayfield <eric@visiblycreative.com>
2018-11-09 17:41:24 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,5 +1,27 @@
 require 'pry'
 
 class Doctor
+  attr_accessor :name, :appointments
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def new_appointment(patient, date)
+    appointment = Appointment.new(patient, self, date)
+    @appointments << appointment
+    appointment
+  end
+
+  def patients
+    @appointments.map { |appointment| appointment.patient }
+  end
+
+  def self.all
+      @@all
+  end
 end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,5 +1,28 @@
 require 'pry'
 
 class Patient
+  attr_accessor :name, :appointments
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def new_appointment(doctor, date)
+    appointment = Appointment.new(self, doctor, date)
+    @appointments << appointment
+    appointment
+  end
+
+  def doctors
+    #Appointment.all.select { |appointment| appointment.patient == self }
+    self.appointments.map { |appointment| appointment.doctor }
+  end
+
+  def self.all
+    @@all
+  end
 end
