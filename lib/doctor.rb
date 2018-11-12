67db45cef45c2837a63eb095fe0e47e233b67311
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-12 17:04:23 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,35 @@
+require "pry"
+
+class Doctor 
+  
+@@all = []  
+attr_accessor :name, :appts, :appt
+
+  def initialize(name)
+    @name = name
+    @appts = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(patient, date)
+    appt = Appointment.new(date, patient, self)
+    appts << appt
+    appt
+  end
+  
+  def appointments
+    @appts.select do |appt|
+    appt.doctor == self
+    end
+  end
+  
+  def patients
+      appointments.collect do |appt|
+      appt.patient
+      end
+  end
+end
